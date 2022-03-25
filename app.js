433a7379973bbb57290f0fc4961fb76f58a24cff
Bingyang Xu <hi@xubingyang.com>
2022-03-25 13:57:11 +0900

    Built the app foundation.

--- app.js
+++ app.js
@@ -1,24 +1,42 @@
+// 加载全局所需的模块
+const path = require('path');
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+require('colors');
+
+// 加载中间件
+const morgan = require('morgan');
+const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({
-      result: 'Homepage route GET',
-      app: 'WhatsApp-Sender-Registration',
-    });
+app.use(express.json());
+
+const senders = [
+  { phoneNumber: 8613011112222 },
+  { phoneNumber: 8613033334444 },
+];
+
+app.get('/api/v1/senders', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: senders.length,
+    data: {
+      senders,
+    },
+  });
 });
 
-app.post('/', (req, res) => {
-  res
-    .status(200)
-    .json({
-      result: 'Homepage route POST',
-      app: 'WhatsApp-Sender-Registration',
-    });
+app.post('/api/v1/senders', (req, res) => {
+  console.log(req.body);
+  res.status(200).send(`Success`);
 });
+
 const port = 5000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}...`);
